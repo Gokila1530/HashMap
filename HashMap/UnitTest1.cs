8ade0e53f55da61b36326fa4830cd74c8a580f11
Gokila1530 <gokila1530@gmail.com>
2023-11-17 11:53:08 +0530

    firstcommit

---
+++ HashMap/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+
+namespace HashMap
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+
+        {
+
+            Hashtable ht = new Hashtable();
+            ht.Add("RollNo", 1);
+            ht.Add("Name", "Bala");
+            ht.Add("Age", 26);
+
+            if (ht.ContainsKey("Name"))
+            {
+                String name = (String)ht["Name"];
+                Console.WriteLine($"Name: {name}");
+            }
+
+           
+
+        }
+    }
+}
